ba13e99cbcca13382c89c9b3c25c2f800648529c
nine623 <94023920+nine623@users.noreply.github.com>
2026-03-06 12:54:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function updateClock(){
 
-const now = new Date();
+const now=new Date();
 
-document.getElementById("time").innerHTML =
-now.toLocaleTimeString();
-
-document.getElementById("date").innerHTML =
-now.toDateString();
+document.getElementById("clock").innerHTML=now.toLocaleTimeString();
 
 }
 
 setInterval(updateClock,1000);
+
+updateClock();
+
+function updateVisitors(){
+
+let count=Math.floor(Math.random()*500)+100;
+
+document.getElementById("visitors").innerText=count;
+
+}
+
+setInterval(updateVisitors,5000);
+
+updateVisitors();
