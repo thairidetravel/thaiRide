fb2c919facbdce0ca75b37affbe8af1ff3199369
nine623 <94023920+nine623@users.noreply.github.com>
2026-03-07 12:53:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+<script>
+
 function updateClock(){
 
 let now = new Date();
@@ -6,16 +8,49 @@ let h = now.getHours().toString().padStart(2,'0');
 let m = now.getMinutes().toString().padStart(2,'0');
 let s = now.getSeconds().toString().padStart(2,'0');
 
-document.getElementById("clock").innerHTML = h+":"+m+":"+s;
+let clock = document.getElementById("clock");
+
+if(clock){
+clock.innerHTML = h + ":" + m + ":" + s;
+}
 
 }
 
+updateClock();
 setInterval(updateClock,1000);
-
 
 
 /* Fake visitor counter */
 
 let count = 120 + Math.floor(Math.random()*80);
 
-document.getElementById("visitors").innerHTML = count;
+let visitors = document.getElementById("visitors");
+
+if(visitors){
+visitors.innerHTML = count;
+}
+
+
+/* Hero Slide */
+
+let slideIndex = 0;
+let slides = document.querySelectorAll(".hero-slide img");
+
+function showSlide(){
+
+slides.forEach(img => img.classList.remove("active"));
+
+slideIndex++;
+
+if(slideIndex > slides.length){
+slideIndex = 1;
+}
+
+slides[slideIndex-1].classList.add("active");
+
+}
+
+showSlide();
+setInterval(showSlide,4000);
+
+</script>
